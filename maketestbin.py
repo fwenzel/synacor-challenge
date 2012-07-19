8ffd3ab7059583a1d12ee8859b4e5b4fddca94af
Fred Wenzel <fwenzel@mozilla.com>
2012-07-19 14:08:30 -0700

    Generate test file from data mentioned in instructions.

---
+++ maketestbin.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Create binary file from input string."""
+import struct
+
+
+IN = '9,32768,32769,4,19,32768'
+
+with open('test.bin', 'wb') as f:
+    codes = map(int, IN.split(','))
+    for c in codes:
+        f.write(struct.pack('<H', c))  # 16-bit unsigned int
